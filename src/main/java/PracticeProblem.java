ba231ff24b13d23ca49a3aca2d1bcf0edd3e816a
KavinKumaran123 <166553090+KavinKumaran123@users.noreply.github.com>
2026-03-05 00:23:37 +0000

    Practise 2

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,33 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+	public static boolean validIndex(int arr[], int a) {
+		try 
+		{
+			int b = arr[a];
+			return true;
+		}
+		catch(ArrayIndexOutOfBoundsException e){
+			return false;
+		}
 
+	}
+	public static int divide(int a, int b){
+		try {
+			int c = a/b;
+			return c;
+		}
+		catch(ArithmeticException e){
+			return 0;
+		}
+	}
+	public static int safeConvertStringtoInt(String a){
+		try {
+			int b = Integer.parseInt(a);
+			return b;
+		}
+		catch(NumberFormatException e){
+			return 0;
+		}
 	}
 
 	
